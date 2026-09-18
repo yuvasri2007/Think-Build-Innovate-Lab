03a32dffef73900437a2252dd41df923d0e2f461
yuvasri2007 <yuvasrie2@gmail.com>
2026-09-18 14:21:19 +0530

    Add files via upload

---
+++ Activity-02_Push_Button/code (1).ino
@@ -0,0 +1,20 @@
+
+const int buttonPin = 2;   
+int buttonState = 0;       
+
+void setup() {
+  pinMode(buttonPin, INPUT);  
+  Serial.begin(9600);         
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);  
+
+  if (buttonState == HIGH) {
+    Serial.println("Button Pressed");
+  } else {
+    Serial.println("Button Released");
+  }
+
+  delay(200);  
+}
